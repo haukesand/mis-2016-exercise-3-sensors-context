c4d18bb9d5f1340f926d6766bfa1782a79b794b9
haukesand <haukesa@web.de>
2016-05-19 21:24:24 +0200

    FFT displayed adjusted for earth gravitation

--- MISassignment3/app/src/main/java/com/example/familiesandhaus/misassignment3/FFTView.java
+++ MISassignment3/app/src/main/java/com/example/familiesandhaus/misassignment3/FFTView.java
@@ -7,17 +7,13 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.NotificationCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
-
 import java.util.Arrays;
 
-
-/**
- * Created by familiesandhaus on 19.05.16.
- */
 public class FFTView extends View {
 
 
@@ -32,9 +28,11 @@ public class FFTView extends View {
     private int windowWidth = 1024;
     public int arrayLCounter = 0;
     private int xTextPos, yTextPos;
-    double sum = 0;
+    double sum, lastSum = 0;
     Paint paint;
     FFT fftObject ;
+
+
 
 
     public void fill(double x) {
@@ -43,32 +41,59 @@ public class FFTView extends View {
         if (arrayLCounter < windowWidth) {
             xBuffer[arrayLCounter] = x;
             arrayLCounter += 1;
-        }else{
-            Arrays.fill(yBuffer,0);
-            fftObject.fft(xBuffer,yBuffer);
-            arrayLCounter = 0;
+        }else if(arrayLCounter == windowWidth){
+                double newxBuffer[] = new double[windowWidth];
+                double newyBuffer[] = new double[windowWidth];
+                System.arraycopy(xBuffer,0,newxBuffer,0,windowWidth);
+                Arrays.fill(yBuffer,0);
+            //calculate FFT and sum up
+            fftObject.fft(newxBuffer,newyBuffer);
             for (int i = 0; i < windowWidth; i++){
-            fftArray[i] = Math.sqrt(xBuffer[i]*xBuffer[i]+yBuffer[i]*yBuffer[i]);
+            fftArray[i] = Math.sqrt(newxBuffer[i]*newxBuffer[i]+newyBuffer[i]*newyBuffer[i]);
             sum += fftArray[i];
             }
-            setupDraw();
-            canvas.drawText(String.valueOf(sum), xTextPos, yTextPos, paint);
+            lastSum = sum/windowWidth;
+           //resetting working variables
+            Arrays.fill(xBuffer,0);
+            Arrays.fill(yBuffer,0);
+            arrayLCounter = 0;
+            sum = 0;
+            //refreshing view
+            invalidate();
         }
 
       }
+
+    public void newRecognizedContext (char myMessage){
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+
+    }
 public void changeWindow (int wWidth){
 
     windowWidth = (int)Math.pow(2,wWidth);
-
+    Arrays.fill(xBuffer,0);
+    Arrays.fill(yBuffer,0);
+    arrayLCounter = 0;
+    sum = 0;
 }
 
-  //  @Override
-   // protected void onFinishInflate(){
-   // super.onFinishInflate();}
+   @Override
+    protected void onFinishInflate(){
+    super.onFinishInflate();
+       setupDraw();
+   }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        if (lastSum == 0)canvas.drawText("Waiting for FFT fill", xTextPos, yTextPos, paint);
+         else
+            canvas.drawText(fmt(lastSum), xTextPos, yTextPos, paint);
+
     }
     private void setupDraw() {
         paint = new Paint();
@@ -80,5 +105,12 @@ public void changeWindow (int wWidth){
         paint.setTextSize(20);
         xTextPos = (canvas.getWidth() / 2);
         yTextPos = (int) ((canvas.getHeight() / 2) - ((paint.descent() + paint.ascent()) / 2)) ;
+    }
 
+    public static String fmt(double d)
+    {
+        if(d == (long) d)
+            return String.format("%d",(long)d);
+        else
+            return String.format("%s",d);
     }}
